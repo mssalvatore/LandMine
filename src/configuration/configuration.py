f5edcd40f37fbd2e829f6c2a52a89ef97ce6fcba
Mike Salvatore <mike.s.salvatore@gmail.com>
2019-07-21 15:51:48 -0400

    Switch configparser -> configobj and incorporate validation
    ConfigObj has built in validation features using the Validator class.
    This commit adds a configspec for basic validation and modifies
    configuration.py to use ConfigOBj and perform basic validation.

--- src/configuration/configuration.py
+++ src/configuration/configuration.py
@@ -1,30 +1,58 @@
-import configparser
+import configobj
+from configobj import ConfigObj
+from validate import Validator
+import os
 
 class ConfigurationError(Exception):
     pass
 
 # TODO: Call validator functions from set_*() methods
 class Configuration:
-    def __init__(self):
-        self.config_file = "./config.ini"
-        self.config = configparser.ConfigParser()
-        self._set_config_defaults()
+    def __init__(self, config_file_path="./config.ini"):
+        self.config_file = config_file_path
+        #self.config = ConfigObj(configspec="./configspec.ini", list_values=True, raise_errors=True)
+        self.config = ConfigObj(self.config_file, configspec="./configspec.ini", list_values=True, raise_errors=True)
+        self.validator = Validator()
+        
+        if os.path.isfile(self.config_file):
+            self.print_errors(self.config.validate(self.validator, copy=True))
 
-    def _set_config_defaults(self):
-        self.config['Logging'] = dict()
-        self.set_landmine_log_path("/var/snap/current/common/landmine.log")
-        self.set_snort_log_path("DUMMY_VALUE")
+    def print_errors(self, results):
+        if results != True:
+            print(results)
+            for (section_list, key, _) in configobj.flatten_errors(self.config, results):
+                if key is not None:
+                    print('The "%s" key in the section "%s" failed validation' % (key, ', '.join(section_list)))
+                else:
+                    print('The following section was missing:%s ' % ', '.join(section_list))
+
+    def validate(self):
+        return self.config.validate(self.validator, copy=True)
+
+    def set_snort_log_path(self, log_path):
+        self.config['Monitoring']['snort_log'] = log_path
 
     def set_landmine_log_path(self, log_path):
         self.config['Logging']['landmine_log'] = log_path
 
-    def set_snort_log_path(self, log_path):
-        self.config['Logging']['snort_log'] = log_path
+    def set_smtp_server(self, server):
+        self.config['Alerting']['smtp_server'] = server
+
+    def set_smtp_port(self, port):
+        self.config['Alerting']['smtp_port'] = port
+
+    def set_smtp_username(self, username):
+        self.config['Alerting']['smtp_username'] = username
+
+    def set_smtp_password(self, password):
+        self.config['Alerting']['smtp_password'] = password
+
+    def set_alert_recipients(self, recipients):
+        self.config['Alerting']['Recipients'] = recipients
  
     def save_config(self):
         try:
-            with open(self.config_file, "w") as cf:
-                self.config.write(cf)
+            self.config.write()
         except Exception as ex:
             msg = "Error writing to config file \"%s\": %s" % (self.config_file, str(ex))
             raise ConfigurationError(msg)
