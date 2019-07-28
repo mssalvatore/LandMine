c95676ab5e86cf5e5880091021724ca1b90fac4a
Mike Salvatore <mike.salvatore@canonical.com>
2019-07-27 21:31:50 -0400

    Call validator functions from set_*() methods

--- src/configuration/configuration.py
+++ src/configuration/configuration.py
@@ -1,5 +1,6 @@
 import configobj
 from configobj import ConfigObj
+import copy
 from email_recipient import EmailRecipient
 from errors import *
 from network_interface import NetworkInterface
@@ -23,17 +24,18 @@ SMTP_SERVER = 'smtp_server'
 SMTP_USERNAME = 'smtp_username'
 SNORT_LOG = 'snort_log'
 
-# TODO: Call validator functions from set_*() methods
 CONFIGSPEC_PATH = os.path.join(os.path.dirname(__file__), "./configspec.ini")
+
 class Configuration:
     def __init__(self, config_file_path="./config.ini"):
         self.config_file = config_file_path
         self.config = ConfigObj(self.config_file, configspec=CONFIGSPEC_PATH,
-                                list_values=True, raise_errors=True)
+                                list_values=True, raise_errors=True,
+                                write_empty_values=True)
         self.validator = Validator()
         
         if os.path.isfile(self.config_file):
-            self.print_errors(self.config.validate(self.validator, copy=True))
+            self.print_errors(self.config.validate(self.validator, copy=True, preserve_errors=True))
 
     def print_errors(self, results):
         if results != True:
@@ -45,7 +47,7 @@ class Configuration:
                     print('The following section was missing:%s ' % ', '.join(section_list))
 
     def validate(self):
-        return self.config.validate(self.validator, copy=True)
+        return self.config.validate(self.validator, copy=True, preserve_errors=True)
 
     @staticmethod
     def _objects_from_comma_separated_list(config_csv, callback):
@@ -99,62 +101,80 @@ class Configuration:
             pass
             # TODO: raise exception
 
+    def _validating_set(self, value, section, key):
+        try:
+            config_copy = copy.deepcopy(self.config)
+            self.config[section][key] = value
+            self._throw_on_invalid_config_option(section, key)
+        except:
+            self.config = copy.deepcopy(config_copy)
+            raise
+
+    def _throw_on_invalid_config_option(self, section, key):
+        results = self.validate()
+
+        if results == True or results[section][key] == True:
+            return
+
+        msg = results[section][key]
+        raise ConfigValidationError(section, key, msg)
+
     def set_snort_log_path(self, log_path):
-        self.config[MONITORING][SNORT_LOG] = log_path
+        self.validating_set(log_path, MONITORING, SNORT_LOG)
 
     def get_snort_log_path(self):
         return self.config[MONITORING][SNORT_LOG]
 
     def set_landmine_log_path(self, log_path):
-        self.config[LOGGING][LANDMINE_LOG] = log_path
+        self.validating_set(log_path, LOGGING, LANDMINE_LOG)
 
     def get_landmine_log_path(self):
         return self.config[LOGGING][LANDMINE_LOG]
 
     def set_smtp_server(self, server):
-        self.config[ALERTING][SMTP_SERVER] = server
+        self.validating_set(server, ALERTING, SMTP_SERVER)
 
     def get_smtp_server(self):
         return self.config[ALERTING][SMTP_SERVER]
 
     def set_smtp_port(self, port):
-        self.config[ALERTING][SMTP_PORT] = port
+        self._validating_set(port, ALERTING, SMTP_PORT)
 
     def get_smtp_port(self):
         return self.config[ALERTING][SMTP_PORT]
 
     def set_smtp_username(self, username):
-        self.config[ALERTING][SMTP_USERNAME] = username
+        self.validating_set(username, ALERTING, SMTP_USERNAME)
 
     def get_smtp_username(self):
         return self.config[ALERTING][SMTP_USERNAME]
 
     def set_smtp_password(self, password):
-        self.config[ALERTING][SMTP_PASSWORD] = password
+        self.validating_set(password, ALERTING, SMTP_PASSWORD)
 
     def get_smtp_password(self):
         return self.config[ALERTING][SMTP_PASSWORD]
 
     def set_alert_recipients(self, recipients):
-        self.config[ALERTING][RECIPIENTS] = recipients
+        self.validating_set(recipients, ALERTING, RECIPIENTS)
 
     def get_alert_recipients(self):
         return Configuration._email_recipients_from_config_list(self.config[ALERTING][RECIPIENTS])
 
     def set_email_subject(self, subject):
-        self.config[ALERTING][EMAIL_SUBJECT] = subject
+        self.validating_set(subject, ALERTING, EMAIL_SUBJECT)
 
     def get_email_subject(self):
         return self.config[ALERTING][EMAIL_SUBJECT]
 
     def set_alert_threshold(self, threshold):
-        self.config[ALERTING][ALERT_THRESHOLD] = threshold
+        self.validating_set(threshold, ALERTING, ALERT_THRESHOLD)
 
     def get_alert_threshold(self):
         return self.config[ALERTING][ALERT_THRESHOLD]
 
     def set_alert_threshold_window_min(self, threshold_window_min):
-        self.config[ALERTING][ALERT_THRESHOLD_WINDOW] = threshold_window_min
+        self.validating_set(threshold_window_min, ALERTING, ALERT_THRESHOLD_WINDOW)
 
     def get_alert_threshold_window_min(self):
         return self.config[ALERTING][ALERT_THRESHOLD_WINDOW]
@@ -163,7 +183,7 @@ class Configuration:
         return self.config[ALERTING][ALERT_THRESHOLD_WINDOW] * 60
 
     def set_network_interfaces(self, interfaces):
-        self.config[MONITORING][NETWORK_INTERFACES] = interfaces
+        self.validating_set(interfaces, MONITORING, NETWORK_INTERFACES)
 
     def get_network_interfaces(self):
         return Configuration._network_interfaces_from_config_list(self.config[MONITORING][NETWORK_INTERFACES])
