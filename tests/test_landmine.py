5a0b3441320eb27650fb74a29f91d8389a1cc16c
Mike Salvatore <mike.s.salvatore@gmail.com>
2019-08-11 14:07:49 -0400

    Add fixture for is_within_time_window() tests

--- tests/test_landmine.py
+++ tests/test_landmine.py
@@ -3,39 +3,30 @@ from landmine import landmine
 from landmine.configuration.email_recipient import EmailRecipient
 import pytest
 
-class TestLandmine:
-    def test_is_within_time_window_star_star(self):
-        dtime = datetime(2019, 8, 9, hour=22, minute=24) # Friday (4)
-        recipient = EmailRecipient("test@test.test", "*", "*")
+@pytest.fixture(scope="module")
+def friday_datetime():
+    return datetime(2019, 8, 9, hour=22, minute=24) # Friday (4)
 
-        assert landmine.is_within_time_window(dtime, recipient)
+def test_is_within_time_window_star_star(friday_datetime):
+    recipient = EmailRecipient("test@test.test", "*", "*")
+    assert landmine.is_within_time_window(friday_datetime, recipient)
 
-    def test_is_within_time_window_day_within_range(self):
-        dtime = datetime(2019, 8, 9, hour=22, minute=24) # Friday (4)
-        recipient = EmailRecipient("test@test.test", "0-4", "*")
+def test_is_within_time_window_day_within_range(friday_datetime):
+    recipient = EmailRecipient("test@test.test", "0-4", "*")
+    assert landmine.is_within_time_window(friday_datetime, recipient)
 
-        assert landmine.is_within_time_window(dtime, recipient)
+def test_is_within_time_window_day_over(friday_datetime):
+    recipient = EmailRecipient("test@test.test", "0-3", "*")
+    assert not landmine.is_within_time_window(friday_datetime, recipient)
 
-    def test_is_within_time_window_day_over(self):
-        dtime = datetime(2019, 8, 9, hour=22, minute=24) # Friday (4)
-        recipient = EmailRecipient("test@test.test", "0-3", "*")
+def test_is_within_time_window_day_under(friday_datetime):
+    recipient = EmailRecipient("test@test.test", "5-6", "*")
+    assert not landmine.is_within_time_window(friday_datetime, recipient)
 
-        assert not landmine.is_within_time_window(dtime, recipient)
+def test_is_within_time_window_day_within_cyclical(friday_datetime):
+    recipient = EmailRecipient("test@test.test", "3-1", "*")
+    assert landmine.is_within_time_window(friday_datetime, recipient)
 
-    def test_is_within_time_window_day_under(self):
-        dtime = datetime(2019, 8, 9, hour=22, minute=24) # Friday (4)
-        recipient = EmailRecipient("test@test.test", "5-6", "*")
-
-        assert not landmine.is_within_time_window(dtime, recipient)
-
-    def test_is_within_time_window_day_within_cyclical(self):
-        dtime = datetime(2019, 8, 9, hour=22, minute=24) # Friday (4)
-        recipient = EmailRecipient("test@test.test", "3-1", "*")
-
-        assert landmine.is_within_time_window(dtime, recipient)
-
-    def test_is_within_time_window_day_outside_cyclical(self):
-        dtime = datetime(2019, 8, 9, hour=22, minute=24) # Friday (4)
-        recipient = EmailRecipient("test@test.test", "5-2", "*")
-
-        assert not landmine.is_within_time_window(dtime, recipient)
+def test_is_within_time_window_day_outside_cyclical(friday_datetime):
+    recipient = EmailRecipient("test@test.test", "5-2", "*")
+    assert not landmine.is_within_time_window(friday_datetime, recipient)
