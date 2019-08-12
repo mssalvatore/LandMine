d53fbea27ce0e8c7a3d0472c6301b1d262face5f
Mike Salvatore <mike.s.salvatore@gmail.com>
2019-08-11 21:45:05 -0400

    Add tests for snort alert parser functions

--- landmine/landmine.py
+++ landmine/landmine.py
@@ -12,7 +12,7 @@ def parse_rule_id(alert_lines):
     if matches:
         return matches.group(1)
 
-    return "";
+    raise Exception("Error parsing rule ID from snort alert")
 
 msg_regex = re.compile(r"\[\*\*\] \[.*\] (.*) \[\*\*\]")
 def parse_alert_msg(alert_lines):
@@ -20,20 +20,33 @@ def parse_alert_msg(alert_lines):
     if matches:
         return matches.group(1)
 
-    return "";
+    raise Exception("Error parsing message from snort alert")
 
 def parse_timestamp(alert_lines):
-    return alert_lines[2].split(' ')[0]
+    timestamp = alert_lines[2].split(' ')[0]
+
+    if not timestamp:
+        raise Exception("Error parsing procol from snort alert")
+
+    return timestamp
 
 def parse_packet_ip_port_direction(alert_lines):
     split_line = alert_lines[2].split(' ');
+    if len(split_line) != 4:
+        raise Exception("Error parsing IP/Port from snort alert")
+
     src = split_line[1]
     direction = split_line[2]
     destination = split_line[3]
     return src + " " + direction + " " + destination;
 
 def parse_protocol(alert_lines):
-    return alert_lines[3].split(' ')[0];
+    protocol = alert_lines[3].split(' ')[0];
+
+    if not protocol:
+        raise Exception("Error parsing procol from snort alert")
+
+    return protocol
 
 def send_email(config, to, message):
     logging.debug("Sending email to %s" % to);
